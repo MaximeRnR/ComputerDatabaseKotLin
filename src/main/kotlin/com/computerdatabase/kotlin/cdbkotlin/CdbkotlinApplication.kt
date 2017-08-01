6781de43a8145d1ac606a217b9b2ab0ca70a81ad
Maxime Reynier <mreynier@excilys.com>
2017-08-01 13:37:04 +0200

    initial commit

---
+++ src/main/kotlin/com/computerdatabase/kotlin/cdbkotlin/CdbkotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.computerdatabase.kotlin.cdbkotlin
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CdbkotlinApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(CdbkotlinApplication::class.java, *args)
+}
